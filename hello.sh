ce2dc94175690be5c2a44410d769cb2906edc4ed
Jana <janarthan.paramanandam@ge.com>
2019-05-03 20:47:40 +0530

    Add my first shell script

--- hello.sh
+++ hello.sh
@@ -6,9 +6,12 @@ for i in `aws s3 ls s3://joy.trivyol.com --profile triv-s3`; do
 
     # echo $i
     if [[ $i == *"png"*  ]]; then
-      echo "It's there !!!"
-      var=$var+1
-      echo $var
+
+      aws s3api put-object --bucket joy.trivyol.com --key $i --content-disposition attachment --content-type image/jpeg --profile triv-s3;
+
+      var=$var+1;
+      echo $var;
+
     fi
 
 done;
